40507ea0f7b50b7044910f7eb09d95d0e28ecdb0
Ahmed <ahmad_bhai28@yahoo.com>
2025-02-22 21:08:34 +0100

    added vehicle header

--- src/Vehicle.h
+++ src/Vehicle.h
@@ -5,4 +5,36 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
-#endif //VEHICLE_H
+#include <string>
+
+class Vehicle {
+public:
+    enum class Type { BICYCLE, CAR, SCOOTER };
+
+    Vehicle(const std::string& id, Type type) : id(id), type(type) {}
+    virtual ~Vehicle() = default;
+
+    std::string getId() const { return id; }
+    Type getType() const { return type; }
+
+private:
+    std::string id;
+    Type type;
+};
+
+class Bicycle : public Vehicle {
+public:
+    Bicycle(const std::string& id) : Vehicle(id, Type::BICYCLE) {}
+};
+
+class Car : public Vehicle {
+public:
+    Car(const std::string& id) : Vehicle(id, Type::CAR) {}
+};
+
+class Scooter : public Vehicle {
+public:
+    Scooter(const std::string& id) : Vehicle(id, Type::SCOOTER) {}
+};
+
+#endif // VEHICLE_H
